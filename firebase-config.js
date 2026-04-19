ed09decf19ab32f1741b52d856e9e104fbb1f256
austriaalvinnoel <austriaalvinnoel@gmail.com>
2026-04-19 08:48:11 +0800

    Harden security and add gallery tab deep-linking

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/*
+  Masters Window Tinting - Firebase Config
+  Fill in this file with your real Firebase Web App values.
+  Keep this file private in your workflow when possible.
+*/
+window.MWT_FIREBASE_CONFIG = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
